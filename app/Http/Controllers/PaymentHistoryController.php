43eb2c1cd97980c444d1f24844516791959351c7
Чайка Илья <Ilya-chayka99@yandex.ru>
2024-11-30 01:13:03 +0400

    upd

--- app/Http/Controllers/PaymentHistoryController.php
+++ app/Http/Controllers/PaymentHistoryController.php
@@ -31,11 +31,13 @@ class PaymentHistoryController extends Controller
         $client = new Client();
         $linkKey = $request->link;
         while (true){
-            $response = $client->post( "http://89.111.131.40:8080/getInfoByPaymentLink?linkKey=$linkKey");
-//                'query' => [
-//                    'linkKey' => $request->link
-//                ]
-//            ]);
+            $response = $client->post( "http://89.111.131.40:8080/getInfoByPaymentLink"
+            , [
+                'form_params' => [
+                    'linkKey' =>  $linkKey,
+
+                ]
+            ]);
             $link = json_decode($response->getBody(), true);
             if($link['state'] == 'error' || $link['paymentStatus'] == -1) return response()->json(['message' => $link['message']], 200);
             if ($link['paymentStatus'] == 1) break;
