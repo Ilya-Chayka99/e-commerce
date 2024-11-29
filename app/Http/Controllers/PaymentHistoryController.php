8b38f26eaad23edffc834aba0bcba4addabd3900
Чайка Илья <Ilya-chayka99@yandex.ru>
2024-11-30 00:31:48 +0400

    upd

--- app/Http/Controllers/PaymentHistoryController.php
+++ app/Http/Controllers/PaymentHistoryController.php
@@ -66,16 +66,18 @@ class PaymentHistoryController extends Controller
             return response()->json(['message' => 'Error not enough money'], 200);
         }
 
-//        $client = new Client();
-//        $response = $client->get( 'http://89.111.131.40:8080/createApiPaymentLink', [
-//            'livingTime' =>  3000000000,
-//            'priceType' => 4,
-//            'price' => $quantity,
-//            'apikey' => 'UGsosAgysjQgWgVL90xWiwOcb1UFxp1yksWj6PErkXCFQ4mIvJUk08mLHwhGbKxsw66UwYztjScgQjHFWbhfrgvs5uMAvUNN6xyvjhnEa93o3dKF6lMtebHN2vJzK2XRahiamc9aoz9Lp6TLtOaakKPv8k6xMqmvfKZPvpozVw4aXdJLtL2p7bPn5sr0hAmTJV3b87UHNR4omNFC8xTVE9l3FEzAv6aSXr4fgYejLPkCGLD9ywUwwvqRH6iUEjX',
-//
-//        ]);
-//        $link = json_decode($response->getBody(), true);
-        return response()->json(['link' => '1233'], 200);
+        $client = new Client();
+        $params = [
+            'livingTime' =>  3000000000,
+            'priceType' => 4,
+            'price' => $quantity,
+            'apikey' => 'UGsosAgysjQgWgVL90xWiwOcb1UFxp1yksWj6PErkXCFQ4mIvJUk08mLHwhGbKxsw66UwYztjScgQjHFWbhfrgvs5uMAvUNN6xyvjhnEa93o3dKF6lMtebHN2vJzK2XRahiamc9aoz9Lp6TLtOaakKPv8k6xMqmvfKZPvpozVw4aXdJLtL2p7bPn5sr0hAmTJV3b87UHNR4omNFC8xTVE9l3FEzAv6aSXr4fgYejLPkCGLD9ywUwwvqRH6iUEjX'
+        ];
+        $response = $client->get( 'http://89.111.131.40:8080/createApiPaymentLink', [
+            'query' => $params
+        ]);
+        $link = json_decode($response->getBody(), true);
+        return response()->json(['link' => $link], 200);
 
     }
 
